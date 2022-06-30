3d9e0b0e907bab0221001aceb0d7c5684bc4e87b
Eric Rivas <92978737+erivas28@users.noreply.github.com>
2022-06-30 13:35:06 -0700

    Time: 20 ms (24.47%), Space: 13.9 MB (91.08%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
-    {
-        if(i<0 || i>=image.size() || j<0 || j>= image[0].size() || image[i][j] == newColor || image[i][j] != val)
-        {
-            return;
-        }
-        image[i][j] = newColor;
-        dfs(image,i-1,j,val,newColor);
-        dfs(image,i+1,j,val,newColor);
-        dfs(image,i,j-1,val,newColor);
-        dfs(image,i,j+1,val,newColor);
-    }
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
-    {
-        int val = image[sr][sc];
-        dfs(image,sr,sc,val,newColor);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+        int old_color = image[sr][sc];
+        
+        dfs(image, sr, sc, color, old_color);
+        
         return image;
     }
+    
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int old_color) {
+        
+        // Skip pixels out of bounds
+        if(sr < 0 || sr >= image.size()) return;
+        if(sc < 0 || sc >= image[0].size()) return;
+        
+        // Skip pixels already the desired color
+        // or that don't match the clicked color
+        if(image[sr][sc] == color) return;
+        if(image[sr][sc] != old_color) return;
+        
+        // Set desired color
+        image[sr][sc] = color;
+        
+        // Recursively explore pixels 4-directionally connected
+        dfs(image, sr, sc + 1, color, old_color); // E
+        dfs(image, sr - 1, sc, color, old_color); // N
+        dfs(image, sr, sc - 1, color, old_color); // W
+        dfs(image, sr + 1, sc, color, old_color); // S
+    }
 };
+
